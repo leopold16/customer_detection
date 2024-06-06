e5bcd8ecb40d80875b419b3bbd8b01e433bffd20
leopold16 <lw2999@columbia.edu>
2024-06-06 16:24:09 +0100

    sample change

--- check.py
+++ check.py
@@ -1,6 +1,1 @@
-import os
-dataset_path = '/Users/leopold/customer_detection/database'
-for root, dirs, files in os.walk(dataset_path):
-    for file in files:
-        if file.endswith(('png', 'jpg', 'jpeg')):
-            print(f"Loading image: {os.path.join(root, file)}")
+print("hello world")
